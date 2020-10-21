cbdeebfa59e3fc875a0c368680ad1f6e1958b92e
Sander Kilen <sander.k.kilen@gmail.com>
2020-10-21 18:26:01 +0200

    Testing

--- processing/dataProcessing.py
+++ processing/dataProcessing.py
@@ -3,9 +3,9 @@ import numpy as np
 import pandas as pd
 from PIL import Image
 
-annotations_path = "C:/dev/ML_Project/data/archive/annotations/Annotation/"
-image_path = "C:/dev/ML_Project/data/archive/images/Images/"
 data_path = "C:/dev/ML_Project/data/"
+annotations_path = data_path + "archive/annotations/Annotation/"
+image_path = data_path + "archive/images/Images/"
 
 def get_paths():
     paths = []
@@ -17,16 +17,29 @@ def get_paths():
     return paths
 
 def save_array(data_array):
-    np.save(data_path + 'data.csv', data_array, allow_pickle=True, fix_imports=True)
+    np.savetxt(data_path + 'data.csv', np.asanyarray(data_array), delimiter=",")
 
 # Convert images from jpg to csv file
 def convert_images():
     paths = get_paths()
     images = []
+    max_length = 0
     for path in paths:
         image = Image.open(image_path + path + ".jpg")
-        data = np.asanyarray(image)
+        data = np.asanyarray(image).flatten()
+        max_length = len(data) if max_length < len(data) else max_length
         images.append(data)
+
+    padded_images = np.zeros([len(images), max_length])
+    for i, j in enumerate(images):
+        padded_images[i][0:len(j)] = j
+
+    lengthOfImage = 0
+    for image in images:
+        if (len(image) > lengthOfImage):
+            lengthOfImage = len(image)
+            print(lengthOfImage)
     save_array(np.asanyarray(images))
+
 
 convert_images()
