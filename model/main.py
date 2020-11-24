c74b334ed4874119068d62d4095f34616f99c897
Sander Kilen <sander.k.kilen@gmail.com>
2020-11-24 16:17:23 +0100

    New main notebook

--- model/main.py
+++ model/main.py
@@ -1,7 +1,11 @@
+from data_processing.dataProcessing import *
 from models.CNN import *
+from models.tuning.CNNHyperModel import *
+from models.MLP import *
+from models.tuning.MLPHyperModel import *
 
 if __name__ == "__main__":
-    #process_data()
+    process_data()
 
     # CNN
     CNN()
